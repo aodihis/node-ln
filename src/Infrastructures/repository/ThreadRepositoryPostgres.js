c3d835c75ecf9707a7a40ba1cdd56e9145f71f3d
Iqbal <aodihis@gmail.com>
2025-04-30 05:20:53 +0700

    * Update the getById thread.

--- src/Infrastructures/repository/ThreadRepositoryPostgres.js
+++ src/Infrastructures/repository/ThreadRepositoryPostgres.js
@@ -1,6 +1,7 @@
 const InvariantError = require('../../Commons/exceptions/InvariantError');
 const ThreadRepository = require('../../Domains/threads/ThreadRepository');
 const NewThread = require('../../Domains/threads/entities/NewThread');
+const Thread = require('../../Domains/threads/entities/Thread');
 
 class ThreadRepositoryPostgres extends ThreadRepository {
     constructor(pool, idGenerator) {
@@ -26,17 +27,42 @@ class ThreadRepositoryPostgres extends ThreadRepository {
 
     async getThreadById(id) {
         const query = {
-            text: 'SELECT * FROM threads WHERE id = $1',
+            text: `
+                SELECT threads.id, threads.title, threads.body, threads.created_at, users.username
+                FROM threads
+                LEFT JOIN users ON threads.owner = users.id
+                WHERE threads.id = $1
+            `,
             values: [id],
-        }
+        };
 
-        const result = await this._pool.query(query);
+        const threadRows = await this._pool.query(query);
 
-        if (!result.rows.length) {
+        if (!threadRows.rows.length) {
             throw new InvariantError("NewThread not found");
         }
 
-        return new NewThread({ ...result.rows[0] });
+        const commentsQuery = {
+            text: `
+                SELECT comments.id as id, users.username as username, comments.created_at as date, comments.content as content
+                FROM comments
+                LEFT JOIN users ON comments.owner = users.id
+                WHERE comments.thread_id = $1
+            `,
+            values: [id],
+        };
+
+        const comments = await this._pool.query(commentsQuery);
+        const thread = {
+            id: threadRows[0].id,
+            title: threadRows[0].title,
+            body: threadRows[0].body,
+            date: threadRows[0].created_at,
+            username: threadRows[0].username,
+            comments: comments,
+        }
+
+        return new Thread(thread);
     }
 }
 
